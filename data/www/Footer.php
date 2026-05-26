af7ba7e36d9f2f3f5d19588500afbeed50530db1
Oto511 <tio.oslak@student.um.si>
2026-05-26 11:16:57 +0200

    naloga7

---
+++ data/www/Footer.php
@@ -0,0 +1,41 @@
+<html lang="sl">
+<footer class="noga py-5">
+    <div class="container">
+        <div class="row pb-3">
+            <div class="col col-lg-4">
+                <h3 class="text-white pb-3">AgroServis</h3>
+                <p>Prefesionalen servis in popravilo kmetijske mehanizacije. Va� zanesljiv partner za vzdr�evanje va�e opreme</p>
+            </div>
+            <div class="col">
+                <h4 class="text-white pb-3">Hitre povezave</h4>
+                <ul class="list-unstyled">
+                    <li><a href="storitve.php" class="povezave text-decoration-none ">Na�e storitve</a></li>
+                    <li><a href="#" class="povezave text-decoration-none ">Naroci servis</a></li>
+                    <li><a href="Onas.php" class="povezave text-decoration-none ">O nas</a></li>
+                    <li><a href="kontakt.php" class="povezave text-decoration-none ">Kontakt</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4 class="text-white pb-3">Kontakt</h4>
+                <ul class="list-unstyled">
+                    <li><a href="https://www.google.com/maps/place/Pri+Habakuku+37,+2000+Maribor/@46.5176564,15.615472,99m/data=!3m1!1e3!4m6!3m5!1s0x476f7830a7211db3:0xe245f32fe8619aa2!8m2!3d46.5176583!4d15.6158286!16s%2Fg%2F11vlsdn3l7?entry=ttu&g_ep=EgoyMDI2MDQyOS4wIKXMDSoASAFQAw%3D%3D" class="povezave text-decoration-none"><i class="bi bi-geo-alt-fill me-2"></i>Pri Habakuku 37, 2000 Maribor</a></li>
+                    <li><a href="tel:+38670468410" class="povezave text-decoration-none"><i class="bi bi-telephone-fill me-2"></i>+386 70 468 410</a></li>
+                    <li><a href="mailto:referat@feri.um" class="povezave text-decoration-none"><i class="bi bi-envelope-fill me-2"></i>referat@feri.um</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4 class="text-white pb-3">Delovni cas</h4>
+                <ul class="list-unstyled">
+                    <li>Ponedeljek-petek: 7:00 - 18:00</li>
+                    <li>Sobota: 7:00-14:00</li>
+                    <li>Nedelja: Zaprto</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <div>
+            <p class="d-flex align-items-center justify-content-center text-white pt-3">� 2026 AgroServis. Vse pravice pridr�ane.</p>
+        </div>
+    </div>
+</footer>
+</html>
---
+++ data/www/Header.php
@@ -0,0 +1,34 @@
+<html lang="sl">
+<nav class="navbar navbar-expand-lg fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand me-auto" href="index.php">Logo</a>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Agroservis</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link mx-lg-2" href="index.php">Domov</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-lg-2" href="storitve.php">Storitve</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-lg-2" href="Onas.php">O nas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-lg-2" href="kontakt.php">Kontakt</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <a href="#" class="button-narociSV">Naroči Servis</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</nav>
+</html>
